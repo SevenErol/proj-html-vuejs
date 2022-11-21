1e1b6e53cadcb276f8a775b67151ee32ae209fcd
SevenErol <lorenzo.maggi1998@gmail.com>
2022-11-21 14:57:56 +0100

    static header complete

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -15,15 +15,15 @@ export default {
 
 <template>
 
-    <header>
+    <header id="site_header">
 
         <div class="container-fluid">
 
             <div class="row">
 
-                <div class="col-3">
+                <div class="col-2 d-flex align-items-center">
                     <div class="row justify-content-center">
-                        <div class="col-4">
+                        <div class="col-6">
                             <img :src="store.getImagePath(store.path_logo)" alt="" class="img-fluid">
                         </div>
                     </div>
@@ -32,16 +32,132 @@ export default {
 
                 <div class="col-6">
 
-                    <nav></nav>
+                    <nav class="navbar navbar-expand-lg">
+                        <div class="container-fluid">
+
+                            <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNavDropdown">
+
+                                <ul class="navbar-nav">
+
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" role="button"
+                                            data-bs-toggle="dropdown" aria-expanded="false">
+                                            Home
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#">Action</a></li>
+                                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                        </ul>
+                                    </li>
+
+
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" role="button"
+                                            data-bs-toggle="dropdown" aria-expanded="false">
+                                            Pages
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#">Action</a></li>
+                                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                        </ul>
+                                    </li>
+
+
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" role="button"
+                                            data-bs-toggle="dropdown" aria-expanded="false">
+                                            Courses
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#">Action</a></li>
+                                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                        </ul>
+                                    </li>
+
+
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" role="button"
+                                            data-bs-toggle="dropdown" aria-expanded="false">
+                                            Features
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#">Action</a></li>
+                                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                        </ul>
+                                    </li>
+
+
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" role="button"
+                                            data-bs-toggle="dropdown" aria-expanded="false">
+                                            Blog
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#">Action</a></li>
+                                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                        </ul>
+                                    </li>
+
+
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" role="button"
+                                            data-bs-toggle="dropdown" aria-expanded="false">
+                                            Shop
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#">Action</a></li>
+                                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                        </ul>
+                                    </li>
+
+
+                                </ul>
+
+                            </div>
+                        </div>
+                    </nav>
 
                 </div>
 
-                <div class="col-3 d-flex">
+                <div class="search_button col-4 d-flex align-items-center">
 
-                    <div class="lang"></div>
-                    <div class="search"></div>
+                    <div class="row">
+                        <div class="col-6 d-flex align-items-center">
+                            <img src="../assets/img/en.png" alt="" class="p-2">
+                            <span class="dropdown p-2">
+                                <a class="dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    English
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Deutschland</a></li>
+                                </ul>
+                            </span>
+
+                            <span>
+                                <font-awesome-icon icon="fa-solid fa-user" />
+                            </span>
+
+
+                        </div>
+
+                        <div class="col-6 d-flex align-items-center p-2">
+                            <input type="text" class="searchbox me-2" placeholder="Search...">
+                            <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+                        </div>
+
+                    </div>
+
 
                 </div>
+
+
 
             </div>
         </div>
